28fbe8383c9d2778708182b07722dd538966b7e1
godlovesyou127888-hub <godlovesyou127888@gmail.com>
2026-07-02 09:58:26 +0800

    Bump app cache for mobile reader

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "renren-100-roots-v1";
+const CACHE_NAME = "renren-100-roots-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -11,6 +11,14 @@ const ASSETS = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
 });
 
 self.addEventListener("fetch", (event) => {
